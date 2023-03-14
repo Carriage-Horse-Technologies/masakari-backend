71669e3f277defed0faec76750911a2d4a38fbda
notch_man <s2113591@klis.tsukuba.ac.jp>
2023-03-14 19:07:42 +0900

    URLの変更に対応

--- api/src/server_status.go
+++ api/src/server_status.go
@@ -69,6 +69,6 @@ func getServerStatus() ([]byte, error) {
 		return nil, err
 	}
 	//キャッシュをセット
-	cacheManager.SaveFor(10*time.Second, KEY_CACHE_STATUS, result_json)
-	return cache_item, nil
+	cacheManager.SaveFor(30*time.Second, KEY_CACHE_STATUS, result_json)
+	return result_json, nil
 }
